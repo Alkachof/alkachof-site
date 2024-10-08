018e1f7b1ea7e52ac33ac36821bdef40c8a99549
Jesus Escobar <leider924@gmail.com>
2024-10-08 18:50:08 -0500

    Landing: Add icon, improve styles

--- src/screens/landing.js
+++ src/screens/landing.js
@@ -5,6 +5,7 @@ import Signup from "../components/modals/signup/signup";
 import useModal from "../hooks/useModal/useModal";
 import ResetPassword from "../components/modals/reset-password/reset-password";
 import Footer from "../components/general/footer/footer";
+import {ReactComponent as Icon} from "../assets/icons/artichoke.svg";	
 
 function Landing() {
   const loginModal = useModal();
@@ -13,27 +14,44 @@ function Landing() {
 
   return (
     <div className="container" role="main">
-      <h1>Alkachof</h1>
-      <p>Tus catalogos en un solo lugar</p>
+      <div className="full-height landing">
+        <Icon />
+        <h1 role="heading">Alkachof</h1>
+        <p className="lead">Tus catalogos en un solo lugar</p>
 
-      <div>
-        <Button text="Ingresar" onClick={loginModal.toggleModal} />
-        <Button text="Registrarse" onClick={signupModal.toggleModal} />
-        <Button
-          text="Recuperar contraseña"
-          onClick={forgotPasswordModal.toggleModal}
+        <div className="space vertical">
+          <Button
+            text="Acceder"
+            onClick={loginModal.toggleModal}
+            type="primary"
+          />
+          <Button
+            text="Crear cuenta"
+            onClick={signupModal.toggleModal}
+            type="secondary"
+          />
+          <Button
+            text="Olvidé mi contraseña"
+            onClick={forgotPasswordModal.toggleModal}
+            type="link"
+          />
+        </div>
+
+        <Signin
+          open={loginModal.showModal}
+          handleClose={loginModal.closeModal}
+          handleForgetPassword={forgotPasswordModal.toggleModal}
+        />
+        <Signup
+          open={signupModal.showModal}
+          handleClose={signupModal.closeModal}
+          handleLogin={loginModal.toggleModal} 
+        />
+        <ResetPassword
+          open={forgotPasswordModal.showModal}
+          handleClose={forgotPasswordModal.closeModal}
         />
       </div>
-
-      <Signin open={loginModal.showModal} handleClose={loginModal.closeModal} />
-      <Signup
-        open={signupModal.showModal}
-        handleClose={signupModal.closeModal}
-      />
-      <ResetPassword
-        open={forgotPasswordModal.showModal}
-        handleClose={forgotPasswordModal.closeModal}
-      />
       <Footer />
     </div>
   );
--- src/screens/landing.test.js
+++ src/screens/landing.test.js
@@ -10,6 +10,19 @@ describe("Landing", () => {
         const landingElement = screen.getByRole("main");
 
         expect(landingElement).toBeInTheDocument();
+    })
 
+    test("Renders signup button", () => { 
+        render(<BrowserRouter><Landing /> </BrowserRouter>);
+        const signupButton = screen.getByText("Crear cuenta");
+
+        expect(signupButton).toBeInTheDocument();
+    })
+
+    test("Renders signin button", () => {
+        render(<BrowserRouter><Landing /> </BrowserRouter>);
+        const signinButton = screen.getByText("Acceder");
+
+        expect(signinButton).toBeInTheDocument();
     })
 })
